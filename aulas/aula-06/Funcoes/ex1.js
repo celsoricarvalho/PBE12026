56734678bb8c03811c11d9d63d213a5d8f27175c
celsoricarvalho <celsoricarvalho@gmail.com>
2026-08-14 12:58:30 -0300

    nv aulas

---
+++ aulas/aula-06/Funcoes/ex1.js
@@ -0,0 +1,8 @@
+// Criando a "maquina" de calcular media
+function calcularMedia(n1, n2) {
+    return (n1 + n2) / 2;
+}
+
+// Usando a maquina
+const resultado = calcularMedia(10, 8);
+console.log(`A media calculada foi: ${resultado}`);
